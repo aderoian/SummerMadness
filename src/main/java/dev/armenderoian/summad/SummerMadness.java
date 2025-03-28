bdcececad43567706147d48f87874e0e306dbe91
Armen Deroian <armendero330@gmail.com>
2025-03-27 22:08:46 -0400

    initial commit

---
+++ src/main/java/dev/armenderoian/summad/SummerMadness.java
@@ -0,0 +1,16 @@
+package dev.armenderoian.summad;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SummerMadness implements ModInitializer {
+
+    public static final String MOD_ID = "summermadness";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/dev/armenderoian/summad/client/SummerMadnessClient.java
@@ -0,0 +1,10 @@
+package dev.armenderoian.summad.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SummerMadnessClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
